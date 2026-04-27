6d5100f878478fe080461140f6080297f6fd6dfc
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-27 11:47:59 +0300

    Update kimi_quotes.py

--- src/kimi_quotes.py
+++ src/kimi_quotes.py
@@ -2,23 +2,26 @@ from requests import Session
 
 class KimiQuotes:
 	def __init__(self) -> None:
-		self.api = "https://kimiquotes.herokuapp.com"
+		self.api = "https://kimiquotes.pages.dev/api"
 		self.session = Session()
 		self.session.headers = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
+	def _get(self, endpoint: str) -> dict:
+		return self.session.get(f"{self.api}{endpoint}").json()
+		
 	def get_all_quotes(self) -> dict:
-		return self.session.get(f"{self.api}/quotes").json()
+		return self._get("/quotes")
 
 	def get_all_quotes_by_year(self, year: int) -> dict:
-		return self.session.get(f"{self.api}/quotes/{year}").json()
+		return self._get(f"/quotes/{year}")
 
 	def get_all_unstamped_quotes(self) -> dict:
-		return self.session.get(f"{self.api}/quotes/unstamped").json()
+		return self._get(f"quotes/unstamped")
 
 	def quote_by_id(self, quote_id: int) -> dict:
-		return self.session.get(f"{self.api}/quote/{quote_id}").json()
+		return self._get(f"/quote/{quote_id}")
 
 	def get_random_quote(self) -> dict:
-		return self.session.get(f"{self.api}/quote").json()
+		return self._get("/quote")
